3b99e9934cd9478c16e27cca6c8d76fd0a972649
DaveToth77 <85111342+DaveToth77@users.noreply.github.com>
2022-01-19 11:43:31 -0500

    added: JSON Server, Delete function

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,5 +1,5 @@
-import { TASKS } from '../../mock-tasks';
 import { Task } from '../../Task';
+import { TaskService } from '../../services/task.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tasks.component.css'],
 })
 export class TasksComponent implements OnInit {
-  tasks: Task[] = TASKS;
+  tasks: Task[] = [];
 
-  constructor() {}
+  constructor(private taskService: TaskService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
+  }
+
+  deleteTask(task: Task) {
+    this.taskService
+      .deleteTask(task)
+      .subscribe(
+        () => (this.tasks = this.tasks.filter((t) => t.id !== task.id))
+      );
+  }
 }
